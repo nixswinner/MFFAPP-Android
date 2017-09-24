af719bbbaab38de1470e4813e8ea70caf8535085
nixswinner <nixswinner@gmail.com>
2017-09-24 12:50:33 +0300

    added SaveSharedPreference class to handle shared preference in the app

---
+++ app/src/main/java/com/tergech/nixon/mffapp/SaveSharedPreference.java
@@ -0,0 +1,78 @@
+package com.tergech.nixon.mffapp;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by Tonui on 6/27/2017.
+ */
+
+public class SaveSharedPreference {
+
+    static final String PREF_USER_NAME= "username";
+    static final String PREF_BMR= "BMR";
+    static final String PREF_GENDER= "username";
+    static final String PREF_PHONE_NUMBER= "phone_number";
+    static final String PREF_SAVE_DELIVERER_ID= "deliverer_id";
+
+
+    static SharedPreferences getSharedPreferences(Context ctx) {
+        return PreferenceManager.getDefaultSharedPreferences(ctx);
+    }
+
+    public static void setUserName(Context ctx, String userName)
+    {
+        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
+        editor.putString(PREF_USER_NAME, userName);
+        editor.commit();
+    }
+
+    //saving deliverer id
+
+    public static void setdeliveryid(Context ctx, String deliver_id)
+    {
+        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
+        editor.putString(PREF_SAVE_DELIVERER_ID, deliver_id);
+        editor.commit();
+    }
+
+    public static void setBMR(Context ctx, String BMR,String Gender)
+    {
+        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
+        editor.putString(PREF_BMR, BMR);
+        editor.commit();
+    }
+    public static void setPhoneNumber(Context ctx, String phone)
+    {
+        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
+        editor.putString(PREF_PHONE_NUMBER, phone);
+        editor.commit();
+    }
+    public static String getUserName(Context ctx)
+    {
+        return getSharedPreferences(ctx).getString(PREF_USER_NAME, "");
+    }
+    //getting delivery id
+    public static String getdeliveryid(Context ctx)
+    {
+        return getSharedPreferences(ctx).getString(PREF_SAVE_DELIVERER_ID, "");
+    }
+
+    public static String getPhoneNumber(Context ctx)
+    {
+        return getSharedPreferences(ctx).getString(PREF_PHONE_NUMBER, "");
+    }
+
+    public static String getBMR(Context ctx)
+    {
+        return getSharedPreferences(ctx).getString(PREF_BMR, "");
+    }
+
+    public static void clearUserName(Context ctx)
+    {
+        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
+        editor.clear(); //clear all stored data
+        editor.commit();
+    }
+}
